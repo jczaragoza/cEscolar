2e6281ad4c0ba99454e55c639540f8c717629626
jczaragoza <castanedadf@gmail.com>
2021-11-10 19:04:42 -0600

    implementando platilla AdminLTE Bootstrap

--- config/settings.py
+++ config/settings.py
@@ -39,7 +39,6 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    # Libs
     'widget_tweaks',
     #Apps
     'core.erp',
--- core/erp/forms.py
+++ core/erp/forms.py
@@ -6,9 +6,6 @@ from core.erp.models import Categorias
 class CategoriaForm(ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        # for form in self.visible_fields():
-        #     form.field.widget.attrs['class'] = 'form-control'
-        #     form.field.widget.attrs['autocomplete'] = 'off'
         self.fields['nom_categoria'].widget.attrs['autofocus'] = True
 
     class Meta:
